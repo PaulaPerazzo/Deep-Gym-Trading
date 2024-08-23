17780052ff87fba531f798d48e770a5a18057dc2
paulaperazzo <mpps@cin.ufpe.br>
2024-08-23 16:54:58 -0300

    feat/ibovespa-tests: tests completed

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=bovesp_pre_pandemic_test
+#SBATCH --job-name=bovesp_all_test
 #SBATCH --ntasks=1
 #SBATCH --mem 16G
 #SBATCH -c 8
 #SBATCH -o job.log
-#SBATCH --output=job_pre_pandemic_output.txt
-#SBATCH --error=job_pre_pandemic_error.txt
+#SBATCH --output=job_all_output.txt
+#SBATCH --error=job_all_error.txt
 
 # carregar versão python
 module load Python/3.8
@@ -14,4 +14,4 @@ module load Python/3.8
 source env/bin/activate
 
 # executar .py
-python3 src/ibovespa/test.py --period pre-pandemic
+python3 src/ibovespa/test.py --period all
