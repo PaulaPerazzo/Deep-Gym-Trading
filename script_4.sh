bb3f67fe8148cc760184a7b32f79aae62df7ac3f
paulaperazzo <mpps@cin.ufpe.br>
2024-09-19 15:05:55 -0300

    feat/cumulative-return: ibovespa

---
+++ script_4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=bovesp_all
+#SBATCH --ntasks=1
+#SBATCH --mem 16G
+#SBATCH -c 8
+#SBATCH -o job.log
+#SBATCH --output=job_all_output.txt
+#SBATCH --error=job_all_error.txt
+
+# carregar versão python
+module load Python/3.8
+
+# ativar ambiente
+source env/bin/activate
+
+# executar .py
+python3 src/ibovespa/test.py --period all
