9a8e32a95979d6b9dbe018d10330eb76a90e5b46
paulaperazzo <mpps@cin.ufpe.br>
2024-10-03 15:06:59 -0300

    feat/b3-tests: run all test

--- src/ibovespa/main.py
+++ src/ibovespa/main.py
@@ -1,16 +1,21 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
+
 from gym_trading.agent import Agent
 from gym_trading.enviroment import PortfolioEnv 
 import pandas as pd
 from gym_trading.network import ActorCritic
+import argparse
 
-def main():
-    stock_data = pd.read_csv("./data/data_cleaned_nan.csv")
+def main(period):
+    stock_data = f"./data/br_stocks_{period}.csv"
+    index_data = f"./data/ibov_{period}.csv"
+
     stock_data = stock_data.set_index("Ticker")
-    index_data = stock_data.copy()
+    index_data = index_data.set_index("Ticker")
     
-    index_data = pd.DataFrame(index_data["^BVSP.4"])
-    stock_data = stock_data.drop(columns=["^BVSP.4"])
-
     min_length = min(len(stock_data), len(index_data))
     stock_data = stock_data[-min_length:]
     index_data = index_data[-min_length:]
@@ -20,13 +25,18 @@ def main():
     
     ### network ###
     HIDDEN_SIZE = 256
-    actor_critic = ActorCritic(2850, len(env.action_space), hidden_size=HIDDEN_SIZE)
+    actor_critic = ActorCritic(860, len(env.action_space), hidden_size=HIDDEN_SIZE)
     optimizer, scheduler =  actor_critic.set_params()
 
     ### agent ###
     agent = Agent(env, actor_critic, optimizer, scheduler)
     agent.train(200)
-    agent.save_model("./models/actor_critic_oficial_2.pth")
+    agent.save_model(f"./models/ibovespa_{period}_2024.pth")
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--period', type=str, required=True, help="pre-pandemic, pandemic, post-pandemic, or all")
+    args = parser.parse_args()
+
+    main(args.period)
